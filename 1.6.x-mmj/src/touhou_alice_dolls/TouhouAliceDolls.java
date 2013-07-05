635e45267ba3e34e592ccb9a33787c90fbd55d77
Iwa-yukinoshita <iwa-yukinoshita@hotmail.co.jp>
2013-07-06 00:28:26 +0900

    ver1.6.1-mmj開発開始

---
+++ 1.6.x-mmj/src/touhou_alice_dolls/TouhouAliceDolls.java
@@ -0,0 +1,49 @@
+package mods.touhou_alice_dolls;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.SidedProxy;
+
+/**
+ * アリスの人形MOD メインクラス
+ */
+@Mod(
+    modid = "touhou_alice_dolls",
+    name = "Alice's Dolls MOD",
+    version = "1.6.x-mmj",
+    dependencies="after:mod_thKaguya"
+    )
+@NetworkMod(
+    clientSideRequired = true,
+    serverSideRequired = false
+    )
+public class TouhouAliceDolls
+{
+    @Instance("touhou_alice_dolls")
+    public TouhouAliceDolls instance;
+    
+    @SidedProxy(
+        clientSide = "mods.touhou_alice_dolls.client.ClientProxy",
+        serverSide = "mods.touhou_alice_dolls.CommonProxy"
+        )
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void  preInit(FMLPreInitializationEvent event)
+    {
+    }
+    @EventHandler
+    public void  init(FMLInitializationEvent event)
+    {
+        proxy.registerRenderers();
+    }
+    @EventHandler
+    public void  postInit(FMLPostInitializationEvent event)
+    {
+    }
+}
---
+++ 1.6.x-mmj/src/touhou_alice_dolls/client/ClientProxy.java
@@ -0,0 +1,17 @@
+package mods.touhou_alice_dolls.client;
+
+import mods.touhou_alice_dolls.CommonProxy;
+
+/**
+ * クライアント側のみの処理を行うクラス
+ */
+public class ClientProxy extends CommonProxy
+{
+    /**
+     * レンダラの登録
+     */
+    @Override
+    public void registerRenderers()
+    {
+    }
+}
