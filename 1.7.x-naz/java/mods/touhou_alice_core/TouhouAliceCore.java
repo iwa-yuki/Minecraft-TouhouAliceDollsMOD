4fd8b20eabfbf01373c85f7415a8aff1f357e1a1
iwa_yuki <iwa-yukinoshita@hotmail.co.jp>
2014-10-25 13:36:32 +0900

    アイテムIDを使わなくなったので削除

--- 1.7.x-naz/java/mods/touhou_alice_core/TouhouAliceCore.java
+++ 1.7.x-naz/java/mods/touhou_alice_core/TouhouAliceCore.java
@@ -79,10 +79,6 @@ public class TouhouAliceCore
 
             this.entityAliceDollID = cfg.get(
                 "entity", "EntityAliceDollID", 68).getInt();
-            this.itemDollCoreID = cfg.get(
-                "item", "itemDollCoreID", 5000).getInt();
-            this.itemAliceDollID = cfg.get(
-                "item", "itemAliceDollID", 5001).getInt();
         }
         catch (Exception e)
         {
@@ -135,12 +131,10 @@ public class TouhouAliceCore
         // ドールコア
         TouhouAliceCore.itemDollCore = new ItemDollCore();
         GameRegistry.registerItem(TouhouAliceCore.itemDollCore, "dollcore", TouhouAliceCore.MODID);
-    	//GameData.getItemRegistry().addObject(itemDollCoreID, "dollcore", this.itemDollCore);
 
         // 人形
         TouhouAliceCore.itemAliceDoll = new ItemAliceDoll();
         GameRegistry.registerItem(TouhouAliceCore.itemAliceDoll, "alicedoll", TouhouAliceCore.MODID);
-        //GameData.getItemRegistry().addObject(itemAliceDollID, "alicedoll", this.itemAliceDoll);
     }
     
     /**
@@ -174,16 +168,6 @@ public class TouhouAliceCore
      * 人形のエンティティID
      */
     private int entityAliceDollID;
-
-    /**
-     * ドールコアのアイテムID
-     */
-    private int itemDollCoreID;
-
-    /**
-     * 人形のアイテムID
-     */
-    private int itemAliceDollID;
 
     /**
      * ドールコアアイテム
