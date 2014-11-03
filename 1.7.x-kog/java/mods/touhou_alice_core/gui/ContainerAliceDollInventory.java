57cade7ce385f3bb17befd6189fdbca964f9c623
iwa_yuki <iwa-yukinoshita@hotmail.co.jp>
2014-11-03 18:23:23 +0900

    コードネーム「小傘」開発開始

---
+++ 1.7.x-kog/java/mods/touhou_alice_core/gui/ContainerAliceDollInventory.java
@@ -0,0 +1,118 @@
+package mods.touhou_alice_core.gui;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import mods.touhou_alice_core.EntityAliceDoll;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemArmor;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+
+public class ContainerAliceDollInventory extends Container {
+
+	private final EntityAliceDoll theDoll;
+    private int numRows;
+	
+	public ContainerAliceDollInventory(EntityPlayer player, World world,
+			EntityAliceDoll doll) {
+		theDoll = doll;
+        numRows = doll.getSizeInventory() / 9;
+        
+		theDoll.openInventory();
+		
+        int i = (this.numRows - 4) * 18;
+        int j;
+        int k;
+
+        for (j = 0; j < this.numRows-1; ++j)
+        {
+            for (k = 0; k < 9; ++k)
+            {
+                this.addSlotToContainer(new Slot(theDoll, k + j * 9, 8 + k * 18, 18 + j * 18));
+            }
+        }            
+        for (k = 0; k < 5; ++k)
+        {
+            this.addSlotToContainer(new Slot(theDoll, k + (this.numRows-1) * 9, 8 + k * 18, 18 + (this.numRows-1) * 18));
+        }
+        for (k = 5; k < 9; ++k)
+        {
+        	final int armorIndex = 8 - k;
+            this.addSlotToContainer(new Slot(theDoll, k + (this.numRows-1) * 9, 8 + k * 18, 18 + (this.numRows-1) * 18)
+            {
+                @SideOnly(Side.CLIENT)
+                public IIcon getBackgroundIconIndex()
+                {
+                    return ItemArmor.func_94602_b(armorIndex);
+                }
+            });
+        }
+
+        for (j = 0; j < 3; ++j)
+        {
+            for (k = 0; k < 9; ++k)
+            {
+                this.addSlotToContainer(new Slot(player.inventory, k + j * 9 + 9, 8 + k * 18, 103 + j * 18 + i));
+            }
+        }
+
+        for (j = 0; j < 9; ++j)
+        {
+            this.addSlotToContainer(new Slot(player.inventory, j, 8 + j * 18, 161 + i));
+        }
+	}
+	
+	@Override
+    public ItemStack transferStackInSlot(EntityPlayer par1EntityPlayer, int par2)
+    {
+        ItemStack itemstack = null;
+        Slot slot = (Slot)this.inventorySlots.get(par2);
+
+        if (slot != null && slot.getHasStack())
+        {
+            ItemStack itemstack1 = slot.getStack();
+            itemstack = itemstack1.copy();
+
+            if (par2 < this.numRows * 9)
+            {
+                if (!this.mergeItemStack(itemstack1, this.numRows * 9, this.inventorySlots.size(), true))
+                {
+                    return null;
+                }
+            }
+            else if (!this.mergeItemStack(itemstack1, 0, this.numRows * 9, false))
+            {
+                return null;
+            }
+
+            if (itemstack1.stackSize == 0)
+            {
+                slot.putStack((ItemStack)null);
+            }
+            else
+            {
+                slot.onSlotChanged();
+            }
+        }
+
+        return itemstack;
+    }
+
+	@Override
+	public boolean canInteractWith(EntityPlayer var1) {
+		return theDoll.isUseableByPlayer(var1);
+	}
+	
+	@Override
+    public void onContainerClosed(EntityPlayer par1EntityPlayer)
+    {
+        super.onContainerClosed(par1EntityPlayer);
+        this.theDoll.closeInventory();
+    }
+	
+	
+}
---
+++ 1.7.x-kog/java/mods/touhou_alice_core/gui/GuiAliceDollInventory.java
@@ -0,0 +1,56 @@
+package mods.touhou_alice_core.gui;
+
+import org.lwjgl.opengl.GL11;
+
+import mods.touhou_alice_core.EntityAliceDoll;
+import mods.touhou_alice_core.TouhouAliceCore;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class GuiAliceDollInventory extends GuiContainer {
+
+	public static final int GuiID = 1;
+    protected static final ResourceLocation inventoryTexture =
+    		new ResourceLocation(TouhouAliceCore.MODID,"textures/gui/inventory.png");
+    private IInventory dollInventory;
+    private IInventory playerInventory;
+    private int inventoryRows;
+
+	public GuiAliceDollInventory(EntityPlayer player, World world,
+			EntityAliceDoll doll) {
+		super(new ContainerAliceDollInventory(player, world, doll));
+		dollInventory = doll;
+		playerInventory = player.inventory;
+        this.allowUserInput = false;
+        short short1 = 222;
+        int i = short1 - 108;
+        this.inventoryRows = dollInventory.getSizeInventory() / 9;
+        this.ySize = i + this.inventoryRows * 18;
+	}
+	
+	@Override
+    protected void drawGuiContainerForegroundLayer(int p_146979_1_, int p_146979_2_)
+    {
+        this.fontRendererObj.drawString(this.dollInventory.hasCustomInventoryName() ? this.dollInventory.getInventoryName() : I18n.format(this.dollInventory.getInventoryName(), new Object[0]), 8, 6, 4210752);
+        this.fontRendererObj.drawString(this.playerInventory.hasCustomInventoryName() ? this.playerInventory.getInventoryName() : I18n.format(this.playerInventory.getInventoryName(), new Object[0]), 8, this.ySize - 96 + 2, 4210752);
+    }
+    
+	@Override
+	protected void drawGuiContainerBackgroundLayer(float var1, int var2,
+			int var3) {
+		
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		mc.renderEngine.bindTexture(inventoryTexture);
+        int k = (this.width - this.xSize) / 2;
+        int l = (this.height - this.ySize) / 2;
+        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.inventoryRows * 18 + 17);
+        this.drawTexturedModalRect(k, l + this.inventoryRows * 18 + 17, 0, 126, this.xSize, 96);
+
+	}
+
+}
